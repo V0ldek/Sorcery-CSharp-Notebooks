5c4b7fa8aa4204cd7afb378c8580114f8fa37d22
Mateusz Gienieczko <mat@gienieczko.com>
2022-05-11 06:53:24 +0200

    06 in progress.

--- 06-EntityFramework/MigrationsExample/DungeonDbContext.cs
+++ 06-EntityFramework/MigrationsExample/DungeonDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace MigrationsExample;
 
@@ -7,14 +6,20 @@ public sealed class DungeonDbContext : DbContext
 {
     public DbSet<DungeonRun> DungeonRuns { get; private init; } = null!;
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        options.UseSqlite("Data Source=../data/Dungeon.db");
+        // Use the password from https://gienieczko.com/teaching/csharp/6-entity-framework/0-database-setup.
+        var connectionString = "server=sql11.freesqldatabase.com;user=sql11491290;password=<PASSWORD>;database=sql11491290";
+        var version = ServerVersion.AutoDetect(connectionString);
+        optionsBuilder.UseMySql(connectionString, version)
+            .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)
+            .EnableSensitiveDataLogging()
+            .EnableDetailedErrors();
     }
 
-    protected override void OnModelCreating(ModelBuilder model)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        var dungeonRun = model.Entity<DungeonRun>();
+        var dungeonRun = modelBuilder.Entity<DungeonRun>();
 
         dungeonRun.HasKey(d => d.Id);
         dungeonRun.ToTable("DungeonRun");
---
+++ 06-EntityFramework/MigrationsExample/Migrations/20220510090410_Initial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrationsExample.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "DungeonRun",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    HeroClass = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    DungeonName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RoomsCleared = table.Column<int>(type: "int", nullable: false),
+                    DamageDealt = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DungeonRun", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DungeonRun");
+        }
+    }
+}
